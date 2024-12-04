212a61da4c8e82c668b84f3e6aba4dc21395db04
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-05 05:01:33 +0700

    Add sort matrix

--- ChoosingAndLoops/ChoosingAndLoops/Program.cs
+++ ChoosingAndLoops/ChoosingAndLoops/Program.cs
@@ -113,16 +113,52 @@
             //    Console.WriteLine();
             //}
 
+            //int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            //int count = 0;
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr.GetLength(1); j++)
+            //    {
+            //        count += arr[i, j] > 0 ? 1 : 0;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-            int count = 0;
+
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                    Console.Write(arr[i, j] + " ");
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    count += arr[i, j] > 0 ? 1 : 0;
+                    for(int k = j + 1; k < arr.GetLength(1); k++)
+                    {
+                        if (arr[i, j] > arr[i, k])
+                        {
+                            int temp = arr[i, j];
+                            arr[i, j] = arr[i, k];
+                            arr[i, k] = temp;
+                        }
+                    }
                 }
             }
-            Console.WriteLine(count);
+
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                    Console.Write(arr[i, j] + " ");
+
+                Console.WriteLine();
+            }
         }
     }
 }
